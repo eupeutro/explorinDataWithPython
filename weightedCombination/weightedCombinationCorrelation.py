467ee8d9cae6a658185ae55b0d28b3c53397a837
Pedro Oliveira <pedro.otavioo9@icloud.com>
2024-11-29 19:27:17 -0300

    update variables name

--- weightedCombination/weightedCombinationCorrelation.py
+++ weightedCombination/weightedCombinationCorrelation.py
@@ -6,10 +6,10 @@ from scipy import stats
 import pandas as pd
 
 #GENERATING RANDOM VARIABLES WITH UNIFORM DISTRIBUTION
-xU = np.random.uniform(low=-3, high=6, size=10000)
+xU10000 = np.random.uniform(low=-3, high=6, size=10000)
 
 #GENERATING RANDOM VARIABLES WITH NORMAL DISTRIBUTION
-xN = np.random.normal(loc=3, scale=2, size=10000)
+xN10000 = np.random.normal(loc=3, scale=2, size=10000)
 
 #GENERATING A WEIGHTED COMBINATION VARIABLE FROM PREVIOUS VARIABLES "xU" AND "xN"
 xSum =2*xN+xU/3
